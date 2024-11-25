3ea6cf4d8c63e1e79e7e8d240ea7d942f64a7752
RK <rahulkar.996@gmail.com>
2024-11-25 21:00:06 +0530

    error-updated

---
+++ frontend/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    presets: [
+      'MernMeal', // This is CRA's default preset
+    ],
+    plugins: [
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  };
+  
