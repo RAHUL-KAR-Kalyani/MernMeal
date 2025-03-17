9c6d6c98d44c2d2b67e474c4ed7451492d9fccb2
Rahul Kar <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2025-03-17 17:21:47 +0530

    Update Home import App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-import Home from './screens/Home';
+import Home from './screens/Home2.jsx';
 import Login from './screens/Login2.jsx';
 import Signup from './screens/Signup2.jsx';
 import { CartProvider } from './components/ContextReducer.jsx';
